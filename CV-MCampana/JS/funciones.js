597e07fbe0e38f1b796290412fa36b58af3020d4
mcampana555 <123955633+mcampana555@users.noreply.github.com>
2023-02-01 23:35:11 -0300

    Add files via upload

---
+++ CV-MCampana/JS/funciones.js
@@ -0,0 +1,17 @@
+document.getElementsById('boton-inf-personal').addEventListener('click', function() {
+    document.getElementById('inf-personal').style.backgroundColor = 'white';
+})
+
+document.getElementsById('boton-experiencias').addEventListener('click', function() {
+    document.getElementById('experiencias').style.backgroundColor = 'white';
+})
+
+document.getElementsById('boton-habilidades').addEventListener('click', function() {
+    document.getElementById('habilidades').style.backgroundColor = 'white';
+})
+
+document.getElementsById('boton-educacion').addEventListener('click', function() {
+    document.getElementById('educacion').style.backgroundColor = 'white';
+})
+
+/* intente que al hacer click en un boton de la barra de navegacion me cambie el color del background del h2 correspondiente pero no pude */
